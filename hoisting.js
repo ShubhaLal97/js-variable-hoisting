db6010c9aa88b505ea71efae37a195c7efb9856c
shubha <shubha07658@gmail.com>
2021-10-28 00:10:05 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,53 @@
+//var test = 10
+//console.log(test)
+
+//test2 = 40
+//console.log(test2)
+//var test2
+
+//test()
+
+//function test() {
+    //console.log('test')
+//}
+
+//var test1 = 10
+//var test2 
+//function test(){
+    //console.log('test')
+//}
+//test2 = 40
+//console.log(test1)
+//console.log(test2)
+//test()
+
+//var x
+//x = "test"
+//console.log(x)
+
+//sum(5,10)
+//sum(12,50)
+
+
+//function sum(num1,num2) {
+    //console.log(num1 + num2)
+//}
+ //sum(4,6)
+
+ 
+ var sum = function(num1,num2){
+     console.log(num1 + num2)
+ }
+
+ sum(4,6)
+ sum(5,10)
+
+
+
+
+
+
+
+
+
+
